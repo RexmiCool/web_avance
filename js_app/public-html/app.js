e610b31a41fcac6ca68cad45941ca836f65172c2
Rexmicool <matthieu.barraud@gmail.com>
2024-06-26 14:31:47 +0200

    keycloak front

--- js_app/public-html/app.js
+++ js_app/public-html/app.js
@@ -1,47 +1,70 @@
-document.getElementById('loginBtn').addEventListener('click', () => {
-    window.location.href = 'http://localhost:8080/auth/google';
-});
-
-document.getElementById('reservationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const flightId = document.getElementById('flightId').value;
-
-    fetch(`http://localhost:8080/fly/${flightId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Vol non trouvé');
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('result').textContent = data;
-        })
-        .catch(error => {
-            document.getElementById('result').textContent = error.message;
-        });
-});
-
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:8080/profile', { credentials: 'include' })
-        .then(response => {
-            if (response.status === 401) {
-                throw new Error('User not authenticated');
+
+    // Configuration de Keycloak
+    const keycloakConfig = {
+        url: 'http://localhost:8085/',
+        realm: 'WebServices',
+        clientId: '1234'
+    };
+
+
+    const keycloak = new Keycloak(keycloakConfig);
+
+    // Initialisation de Keycloak
+    keycloak.init({
+        onLoad: 'login-required'
+    });/*.then(authenticated => {
+        if (authenticated) {
+            console.log('Utilisateur authentifié');
+            updateUserProfile();
+        } else {
+            console.log('Utilisateur non authentifié');
+        }
+    }).catch(error => {
+        console.error('Erreur d\'initialisation de Keycloak', error);
+    });*/
+
+    document.getElementById('loginBtn').addEventListener('click', () => {
+        keycloak.login();
+    });
+
+    document.getElementById('reservationForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const flightId = document.getElementById('flightId').value;
+
+        fetch(`http://localhost:8080/fly/${flightId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${keycloak.token}`
             }
-            return response.json();
         })
-        .then(user => {
-            const userProfile = document.getElementById('userProfile');
-            userProfile.innerHTML = `
-                <h2>Profil utilisateur</h2>
-                <div>
-                    <p>Nom complet : ${user.displayName}</p>
-                    <p>Email : ${user.email}</p>
-                    <img src="${user.photoUrl}" alt="Photo de profil"><br/>
-                </div>
-                <button><a href="http://localhost:8080/logout">Se déconnecter</a></button>
-            `;
-        })
-        .catch(error => {
-            console.error('Erreur lors de la récupération du profil utilisateur :', error.message);
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Vol non trouvé');
+                }
+                return response.json();
+            })
+            .then(data => {
+                document.getElementById('result').textContent = JSON.stringify(data);
+            })
+            .catch(error => {
+                document.getElementById('result').textContent = error.message;
+            });
+    });
+
+    function updateUserProfile() {
+        const userProfile = document.getElementById('userProfile');
+        userProfile.innerHTML = `
+            <h2>Profil utilisateur</h2>
+            <div>
+                <p>Nom complet : ${keycloak.tokenParsed.name}</p>
+                <p>Email : ${keycloak.tokenParsed.email}</p>
+            </div>
+            <button id="logoutBtn">Se déconnecter</button>
+        `;
+
+        document.getElementById('logoutBtn').addEventListener('click', () => {
+            keycloak.logout();
         });
+    }
 });
